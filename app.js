db0a63732bd7999bb901fb8f85ac56aac775a8b7
Martin Facorro <martin.facorro@accenture.com>
2023-12-11 18:54:50 -0300

    Implement inquirerMenu

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
+const { inquirerMenu } = require('./helpers/inquirer');
+
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
 const main = async()=>
 {
-    console.log('Hola Mundo');
-
     let opt ='';
-
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({opt});
 
-        if (opt !== '0' ) await pausa();
-        
     } while(opt !== '0')
-
 }
 
 main();
---
+++ helpers/inquirer.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const prompt = inquirer.createPromptModule();
+
+const preguntas = [
+    {
+        type : 'list',
+        name: 'opcion',
+        message: 'Que desea hacer?',
+        choices: ['opt1','opt2','opt3']
+    }
+];
+
+const inquirerMenu = async() => {
+    console.clear();
+    console.log('========================='.green);
+    console.log(' Seleccione una opcion'.green);
+    console.log('=========================\n'.green);
+    const opt = await inquirer.prompt(preguntas);
+
+    return opt;
+
+}
+
+module.exports = {
+    inquirerMenu
+}
